9db3a757497defc08248b6078eb6e5b0123a7b9b
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-10-31 22:54:14 +0100

    deploy

--- script/DeployMerkleAirdrop.s.sol
+++ script/DeployMerkleAirdrop.s.sol
@@ -9,14 +9,14 @@ import {console} from "forge-std/console.sol";
 contract DeployMerkleAirdrop is Script {
     bytes32 public ROOT =
         0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
-    // 4 users, 25 Bagel tokens each
+    // 4 users, 25 Bagel tokens each for real
     uint256 public AMOUNT_TO_TRANSFER = 4 * (25 * 1e18);
 
-    function deployMerkleAirdrop() public returns(MerkleAirdrop,BagelToken){
+    function deployMerkleAirdrop() public returns (MerkleAirdrop, BagelToken) {
         vm.startBroadcast();
 
         BagelToken BagelToken = new BagelToken();
 
-        MerkleAirdrop airdrop = new MerkleAirdrop(ROOT,IERC20(bagelToken));
+        MerkleAirdrop airdrop = new MerkleAirdrop(ROOT, IERC20(bagelToken));
     }
 }
